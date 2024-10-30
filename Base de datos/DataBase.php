bc548067021f4e863c1b6903db3635fbb6ebfe47
Kaii Lawliet <jaret.gonzalez.ct59@gmail.com>
2024-10-29 21:00:10 -0600

    Cambio de conexion a local

--- Base de datos/DataBase.php
+++ Base de datos/DataBase.php
@@ -1,6 +1,6 @@
 <?php
 $host = 'localhost'; // Cambia esto si tu servidor no está en localhost
-$dbname = 'RegistroLogistico'; // Nombre de la base de datos
+$dbname = 'RastreoLogistico'; // Nombre de la base de datos
 $user = ''; // Tu nombre de usuario de MySQL
 $password = ''; // Tu contraseña de MySQL
 
