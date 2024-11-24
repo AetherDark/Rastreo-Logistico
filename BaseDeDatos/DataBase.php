7284e074527869f9e38c52c6db9f38655feed9e2
Kaii Lawliet <jaret.gonzalez.ct59@gmail.com>
2024-11-24 13:40:32 -0600

    Actualizacion de conecion de base de datos

--- BaseDeDatos/DataBase.php
+++ BaseDeDatos/DataBase.php
@@ -1,5 +1,5 @@
 <?php
-$host = 'b0aksj8vnbgrjqf1hnl6-mysql.services.clever-cloud.com'; // Conexion a base de datos de manera local en MySQL
+$host = 'b0aksj8vnbgrjqf1hnl6-mysql.services.clever-cloud.com'; // Conexion a base de datos de remota
 $dbname = 'b0aksj8vnbgrjqf1hnl6'; // Nombre de la base de datos
 $user = 'udyqkxeukqobm1ci'; // Usuario para conectar a la base de datos
 $password = 'Ro5jIojLNbBuTQoiMWfx'; // Contraseña del computador, cambiar dependiendo de quien la use
